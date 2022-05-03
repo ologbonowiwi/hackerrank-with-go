7945b31f82153268d1ed1ddd50cf8e6456d2d5d6
Wesley Matos <wm4tos.777@gmail.com>
2022-05-02 23:24:58 -0300

    feat: solve staircase question

---
+++ staircase/staircase.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func buildStarcaise(n int32) []string {
+	stairs := make([]string, n)
+
+	for index := range stairs {
+		stairs[index] = strings.Repeat("#", index+1)
+	}
+
+	return stairs
+}
+
+/*
+ * Complete the 'staircase' function below.
+ *
+ * The function accepts INTEGER n as parameter.
+ */
+
+func staircase(n int32) {
+	// Write your code here
+	stairs := buildStarcaise(n)
+
+	for _, stair := range stairs {
+		fmt.Println(stair)
+	}
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	nTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	staircase(n)
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ staircase/staircase_test.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_buildStarcaise(t *testing.T) {
+	type args struct {
+		n int32
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{
+			name: "example 1",
+			args: args{
+				n: 4,
+			},
+			want: []string{"#", "##", "###", "####"},
+		},
+		{
+			name: "example 2",
+			args: args{
+				n: 10,
+			},
+			want: []string{"#", "##", "###", "####", "#####", "######", "#######", "########", "#########", "##########"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := buildStarcaise(tt.args.n); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("buildStarcaise() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
