be524877f5d2ad5688ae72c0dede7855d0e41c40
Wesley Matos <wm4tos.777@gmail.com>
2022-05-07 19:39:08 -0300

    test: add performance tests

--- time-complexity-primality/time-complexity-primality_test.go
+++ time-complexity-primality/time-complexity-primality_test.go
@@ -1,16 +1,21 @@
 package main
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
+
+type args struct {
+	n int32
+}
+type TestCase struct {
+	name string
+	args args
+	want string
+}
 
 func Test_primality(t *testing.T) {
-	type args struct {
-		n int32
-	}
-	tests := []struct {
-		name string
-		args args
-		want string
-	}{
+	tests := []TestCase{
 		{
 			name: "Case 1",
 			args: args{
@@ -44,6 +49,27 @@ func Test_primality(t *testing.T) {
 			args: args{
 				n: 1,
 			},
+			want: "Not prime",
+		},
+		{
+			name: "Case 6",
+			args: args{
+				n: 9891,
+			},
+			want: "Not prime",
+		},
+		{
+			name: "Case 7",
+			args: args{
+				n: 2,
+			},
+			want: "Prime",
+		},
+		{
+			name: "Case 8",
+			args: args{
+				n: 3,
+			},
 			want: "Prime",
 		},
 		// TODO: Add test cases.
@@ -54,5 +80,43 @@ func Test_primality(t *testing.T) {
 				t.Errorf("primality() = %v, want %v", got, tt.want)
 			}
 		})
+	}
+}
+
+func TestWithTimeOut(t *testing.T) {
+	timeout := time.After(10 * time.Second)
+	done := make(chan bool)
+	go func() {
+		tests := []TestCase{
+			{
+				name: "Big O Case 1 — should pass in less of 4 seconds",
+				args: args{
+					n: 1000000007,
+				},
+				want: "Prime",
+			},
+			{
+				name: "Big O Case 2 — should pass in less of 4 seconds",
+				args: args{
+					n: 1000000009,
+				},
+				want: "Prime",
+			},
+		}
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				if got := primality(tt.args.n); got != tt.want {
+					t.Errorf("primality() = %v, want %v", got, tt.want)
+				}
+			})
+		}
+		time.Sleep(5 * time.Second)
+		done <- true
+	}()
+
+	select {
+	case <-timeout:
+		t.Fatal("Test didn't finish in time")
+	case <-done:
 	}
 }
